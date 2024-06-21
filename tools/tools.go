75d58f988cc7f2b1ef01078dbfdb994ae7f13ab3
Vitaly Shatskikh <vvsh.msk@gmail.com>
2024-06-21 19:47:02 +0300

    update tools

--- tools/tools.go
+++ tools/tools.go
@@ -5,8 +5,8 @@ package tools
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/michurin/human-readable-json-logging/cmd/pplog"
 	_ "github.com/vektra/mockery/v2"
-	_ "github.com/vitalyshatskikh/human-readable-json-logging/cmd/pplog"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "gotest.tools/gotestsum"
 )
